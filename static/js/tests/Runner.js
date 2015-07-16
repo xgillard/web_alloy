6908c7319a6bdc937f033025f14ea01c60c3deec
Xavier GILLARD <xaviergillard@gmail.com>
2015-07-15 21:31:28 -0400

    respawn test fwk

--- static/js/tests/Runner.js
+++ static/js/tests/Runner.js
@@ -1,15 +1,22 @@
-define(['qunit', 'jquery', 'underscore'], function(QUnit, $, _){
+define(['qunit'], function(QUnit){
     
     function Runner(Libs){
         var self = this;
         Libs.forEach(function(t){self.run(t);});
+        
         QUnit.start();
     }
     
     Runner.prototype.run = function(Lib){
-        var tests = _.keys(Lib);
+        var suite = Lib.SuiteInfo;
+        QUnit.module(suite.title, {
+            beforeEach: suite.setup,
+            afterEach : suite.tearDown
+        });
+        
+        var tests = Object.keys(Lib.TestCases);
         tests.forEach(function(test){
-            QUnit.test(test, Lib[test]);
+            QUnit.test(test, Lib.TestCases[test]);
         });
     };
     
