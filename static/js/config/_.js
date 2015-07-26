728d1c0c836f7ec72a3a2c8956537f498fcb6b94
Xavier GILLARD <xaviergillard@gmail.com>
2015-07-25 23:42:24 -0400

    flip flop

--- static/js/config/_.js
+++ static/js/config/_.js
@@ -1,11 +1,13 @@
 define(
   [
+  'config/ConfigType',
   'config/Configuration','config/Fonts', 'config/Palettes', 'config/Layouts', 
   'config/Shapes', 'config/Borders', 'config/Signature',
   'config/ui/_'
   ],
-  function(Configuration, Fonts, Palettes, Layouts, Shapes, Borders, Sig, confui){
+  function(ConfigType, Configuration, Fonts, Palettes, Layouts, Shapes, Borders, Sig, confui){
       return {
+          ConfigType : ConfigType,
           Config     : Configuration,
           Fonts      : Fonts,
           Palettes   : Palettes,
--- static/js/ui/FlipFlop.js
+++ static/js/ui/FlipFlop.js
@@ -6,10 +6,10 @@ define(
         this.active   = on;
         this.inactive = off;
         
-        this.checkbox = $("<input type='checkbox' style='visibility: hidden' checked='false' />");
         this.tag      = $("<label class='btn btn-default'></label>");
         this.span     = $("<span style='margin-left: -.75em'>"+off+"</span>");
-
+        this.checkbox = $("<input type='checkbox' style='visibility: hidden' checked='false' />");
+        
         this.tag.append(this.checkbox);
         this.tag.append(this.span);
         
@@ -32,12 +32,10 @@ define(
       self.checkbox.prop('checked', value);
       if(value){
         self.span.text(self.active);
-        self.tag.removeClass("btn-default");
-        self.tag.addClass("btn-primary");
+        self.tag.addClass("btn-primary").removeClass("btn-default");
       } else {
         self.span.text(self.inactive);
-        self.tag.addClass("btn-info");
-        self.tag.removeClass("btn-default");
+        self.tag.addClass("btn-default").removeClass("btn-primary");
       } 
     };
     
