907a18f7e4a090016870548071886b4026e97b3d
Xavier GILLARD <xaviergillard@gmail.com>
2015-08-04 02:11:21 -0400

    subgraph highlighting

--- static/js/alloy/ui/InstanceView.js
+++ static/js/alloy/ui/InstanceView.js
@@ -30,19 +30,73 @@ define(
                 .attr("height","100%");
         
         self.tag.append(self.viztoolbar.tag);
-        
-        
+        enable_zoom(self);
+        enable_highlighting(self);
+      };
+      
+      // ZOOMING
+      function enable_zoom(self){
         var svg  = d3.select(self.tag[0]).select("svg");
         var g    = svg.select("g");
         var zoom = d3.behavior.zoom()
                     .translate(d3.transform(g.attr("transform")).translate)
                     .on("zoom", _.partial(zoomed, g));
-        svg.call(zoom);
+        svg.call(zoom);  
+      };
+      function zoomed(g){
+        g.attr("transform", "translate(" + d3.event.translate + ")" + " scale(" + d3.event.scale + ")");
       };
       
-      function zoomed(g){
-          g.attr("transform", "translate(" + d3.event.translate + ")" + " scale(" + d3.event.scale + ")");
-      }
+      // HIGHLIGHTING
+      function enable_highlighting(self){
+        var a_bysname = _.indexBy(self.instance.atoms, function(a){
+           return a.simple_atomname(); 
+        });
+        var a_byname  = _.indexBy(self.instance.atoms, function(a){
+           return a.atomname; 
+        });
+        
+        // whenever you're over a node
+        self.tag.find("svg g.node").on("mouseover", function(e){
+            var title = $(this).find("title").text(); // simple_atomname
+            var atom  = a_bysname[title];
+            
+            // start by disabling everything
+            self.tag.find("svg g.node").css({'opacity':'.1'});
+            self.tag.find("svg g.edge").css({'opacity':'.1'});
+            
+            // reinstate myself
+            $(this).css({'opacity':'1'});
+            // reinstate all outgoing edges + nodes
+            var iam_src = _.where(self.instance.tuples, {src: atom.atomname});
+            _.each(iam_src, function(t){
+                var other_end = a_byname[t.dst].simple_atomname();
+                $("svg .node").filter(function(i, n){
+                    return $(n).find("title").text() === other_end;
+                }).css({'opacity':'1'});
+                $("svg .edge").filter(function(i, n){
+                    return $(n).find("title").text() === title+"->"+other_end;
+                }).css({'opacity':'1'});
+            });
+            // reinstate all incoming edges + nodes
+            var iam_dst = _.where(self.instance.tuples, {dst: atom.atomname});
+            _.each(iam_dst, function(t){
+                var other_end = a_byname[t.src].simple_atomname();
+                $("svg .node").filter(function(i, n){
+                    return $(n).find("title").text() === other_end;
+                }).css({'opacity':'1'});
+                $("svg .edge").filter(function(i, n){
+                    return $(n).find("title").text() === other_end+"->"+title;
+                }).css({'opacity':'1'});
+            });
+        });
+        // Whenever you leave: reset everything
+        self.tag.find("svg g.node").on("mouseout", function(e){
+            self.tag.find("svg g.node").css({'opacity':'1'});
+            self.tag.find("svg g.edge").css({'opacity':'1'});
+        });
+        
+      };
       
       return InstanceView;
   }
