7aaef3fdcc98460402e4fffe7884bf29a1fd70b3
Xavier GILLARD <xaviergillard@gmail.com>
2015-08-17 17:42:50 -0400

    Undo 'Save processing': I like to be able to refresh the page without losing its content

--- frontend/js/view/general/ShareDialog.js
+++ frontend/js/view/general/ShareDialog.js
@@ -1,32 +1,31 @@
 define(['jquery', 'util/_','bootstrap'], function($, _){
     
-    $.fn.shareDialog = function(app){
-      var content  = mkContent();
-      
+    $.fn.shareDialog = function(){
       this.popover({
         title    : "Share your work using the following URL",
         html     : true,
         container: $(document.body),
         placement: 'bottom',
-        content  : content
-      });
-      
-      this.on('show.bs.popover', function() { 
-         content.find('input').val(mkUrl(app));
-      });
+        content  : mkContent()
+     });
     };
     
     function mkContent(){
-        var div = $("<div><input style='width: 100%' type'text'></input></div>");
+        var div = $("<div></div>");
         div.css({width: '300px'});
+        refresh(div);
+        $(window).on("hashchange", _.partial(refresh, div));
         return div;
     };
     
-    function mkUrl(app){
-      var url       = ""+window.top.location;
-      var beginning = url.split('!')[0];
-      var encoded   = app.encode();
-      return beginning+'!'+encoded;
+    function mkInput(){
+        var txt = $("<input type'text'></input>").val(window.top.location);
+        txt.css({width: '300px'});
+        return txt;
+    };
+    
+    function refresh(self){
+        self.html(mkInput());  
     };
     
     return $;
