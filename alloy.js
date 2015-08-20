8a793f2131dec7b17874534054d884dba3adccfd
Xavier GILLARD <xaviergillard@gmail.com>
2015-08-20 15:43:05 -0400

    backend component documented

--- alloy.js
+++ alloy.js
@@ -1,3 +1,27 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015 Xavier Gillard
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
 /**
  * This is the main 'server' that will back the web based Alloy Analyzer implementation
  */
@@ -29,24 +53,42 @@ app.get("/", function(request, response){
   response.sendFile(__dirname + "/frontend/index.html");
 });
 
+// This connects each client to the server using socket.io Websocket api which allows you
+// to have a bi-directional communication between the client and the server.
 io.on('connection', function(socket){
+   // This socket context is actually all we need to know about the client. 
+   // It serves as a 'bag' to store temp. information about one particular client for one 
+   // particuliar computation.
    var socket_context = {id: socket.id};
    
+   // This function is used as a callback that we'll pass on the 'resolution.find_instance'
+   // method. Its purpose is to set the user (socket) id in the response that was found then
+   // emit the result so that client can get it.
+   // I have chosen to implement it this way because I wanted to keep the concerns separated:
+   // this module (alloy.js) is responsible for the client interactions (which includes the
+   // event emission) whereas the other modules should only care about their own computation.
+   //
+   // Please note, that even though the function is called 'instance_found', it is that very
+   // same method that gets executed when the computation fails (for whatever reason)
    var instance_found = function(result){
      var emission = {sock_id: socket.id, result: result};
      io.emit("instance_found", emission);
    };
    
+   // This is what gets executed when an user chooses to 'execute' his model.
    socket.on('find_instance', function(model){
      resolution.find_instance(model, socket_context, instance_found);
    });
    
+   // This is what gets executed when an user decides that after all the computation is taking
+   // too long and must be aborted
    socket.on('abort_execution', function(){
      resolution.abort(socket_context);
    });
    
    // If the client is leaving the page, we won't be able to send him the response he
    // wants anyway. So there really is no point in continuing to do computation for him.
+   // NOTE: this event is triggered automatically by socket.io when the user leaves the page
    socket.on('disconnect', function(){
      resolution.abort(socket_context);
    });
--- instance_resolution.js
+++ instance_resolution.js
@@ -1,3 +1,32 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015 Xavier Gillard
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+/**
+ * This module takes care of building the bridge between a4cli and a request
+ * received by the backend (alloy.js)
+ */
+
 // This is to be able to spawn the alloy subprocess
 var async      = require('async');
 var fs         = require('fs');
@@ -57,6 +86,11 @@ function find_instance(model, context, whendone){
   });
 };
 
+/**
+ * This is the function to use in case an user wants to kill the task being carried out for him.
+ * Please note that this can be called without risk regarding of the fact that a user may or may not$
+ * have a task ongoing.
+ */
 function abort(context){
    if( context.execution ){
        context.execution.kill(/*SIGTERM*/);
@@ -68,6 +102,10 @@ function handle_file_error(err, callback){
   handle_error("There was a problem writing your model to disk", callback);
 }
 
+/**
+ * This creates an appropriate error response (formatted as that of a4cli) and then passes it on to the
+ * callback. So that the caller knows that his computation has failed and why it has.
+ */
 function handle_error(err, callback){
   var err_rsp = {
     isError : true,
@@ -80,12 +118,20 @@ function handle_error(err, callback){
   callback(JSON.stringify(err_rsp));  
 };
 
-      
+/**
+ * This function determines the name of a module based on its content. It does so, by simply trying to find and parse
+ * the 'module xxx' statement.
+ *
+ * Please note that this has been copied over from the frontend code, so you might want to keep these two version in sync
+ * whenever you change the other (or you might want to find a clever way to serve that same function to both the backend
+ * and the frontend).
+ */
 function module_title(module){
    var exp = /module\s+([^\s]+)/;
    var ret = exp.exec(module);
    return !ret || ret.length <2 ? 'Untitled' : ret[1];
 };
 
+// These two statements are basically Node.js way to say what functions are to be considered public.
 exports.find_instance = find_instance;
 exports.abort         = abort;
