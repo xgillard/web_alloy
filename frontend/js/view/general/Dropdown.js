f2a1220aa8a8465e28249e47a54445b467673637
Xavier GILLARD <xaviergillard@gmail.com>
2015-08-21 17:31:15 -0400

    Now all the code has been commented.

--- frontend/js/view/general/Dropdown.js
+++ frontend/js/view/general/Dropdown.js
@@ -1,5 +1,33 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015 Xavier Gillard
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+/**
+ * This module defines a Bootstrap dropdown box widget. It is used i-e to implement
+ * the AtomNav from the view/config package
+ */
 define(['jquery', 'util/_','bootstrap'], function($, _){
-    
+    // constructor
     function Dropdown(options, callback, label){
       this._options= options;
       this._label  = label || null;
@@ -13,7 +41,7 @@ define(['jquery', 'util/_','bootstrap'], function($, _){
       this.tag.append(this.button);
       this.tag.append(this.drop);
     };
-    
+    // returns the value that is displayed as 'selected' in the dropdown
     Dropdown.prototype.val = function(){
         if(arguments.length>0){
             if(this.value === arguments[0]) return;
@@ -24,28 +52,33 @@ define(['jquery', 'util/_','bootstrap'], function($, _){
         }
         return this.value;
     };
-    
+    // returns the options that have been made available for selection
     Dropdown.prototype.options = function(){
        return this._options;
     };
-    
+    // changes the button label upon selection change
     function updatingButtonLabel(self, val){
        self.val(val);
        self.callback(val);
     };
-    
+    // makes a BS button (which is the component used to implement the BS dropdown)
     function mkButton(text){
         return $(
         "<button type='button' class='btn btn-default dropdown-toggle' data-toggle='dropdown'>" +
           mkButtonText(text) + 
         "</button>");
     };
+    // makes the button text, making sure it ends up with a caret at the end. This caret is 
+    // the visual hint given to the user telling him that after all, this button is going to 
+    // behave as a select and not as a regular button.
     function mkButtonText(text){
        return text + mkCaret(); 
     };
+    // Returns a caret
     function mkCaret(){
       return "<span class='caret' style='margin-left: 1em'></span>";
     };
+    // creates the dropdown menu that is toggled when the user clicks on the componnent
     function mkDrop(options, callback){
         var drop = $("<ul class='dropdown-menu'></ul>");
         _.each(options, function(opt){
@@ -53,6 +86,8 @@ define(['jquery', 'util/_','bootstrap'], function($, _){
         });
         return drop;
     };
+    // creates the visual elements that permit the display of the different options to be 
+    // proposed.
     function mkOption(option,callback){
         var link = $("<a style='cursor: pointer'>"+option+'</a>');
         var opt  = $("<li></li>").append(link);
--- frontend/js/view/general/MultiEditor.js
+++ frontend/js/view/general/MultiEditor.js
@@ -1,7 +1,38 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015 Xavier Gillard
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+/**
+ * This module defines a multi editor. That is to say, one that offers the possibility
+ * to edit multiple documents at the same time.
+ * This multi editor is composed of a certain amount of editors (amongst which only one is 
+ * visible) and a navigator pane that allows the user to switch from one document to the 
+ * other.
+ */
 define(
   ['jquery', 'util/_', 'view/general/Editor', 'model/AppContext'],
   function($,_, Editor){
-      
+      // constructor
       function MultiEditor(app){
         this.app         = app;
         this.tag         = mkTag();
@@ -14,22 +45,23 @@ define(
         
         $(app).on("changed:modules", _.partial(update, this));
       };
-      
+      // returns the editor that is currently being viewed by the user.
       MultiEditor.prototype.currentEditor = function(){
         var mod = this.app.current_module;
         return this.editors[mod];  
       };
+      // this removes all error markers from all modules
       MultiEditor.prototype.clearErrors = function(){
         this.navigator.find("span.badge").remove();  
         _.each(this.editors, function(ed){
            ed.getSession().setAnnotations([]); 
         });
       };
-      
+      // this shows or hide the navigation panel on the left
       MultiEditor.prototype.toggle_navigator = function(){
         this.tag.find(".hidable").toggleClass('invisible'); 
       };
-      
+      // This goes through all the errors and warnings and reports all of these for each of the modules being edited.
       MultiEditor.prototype.reportErrors = function(warnings, errors){
         var self   = this;
         var titles = _.map(this.app.modules, module_title);
@@ -51,7 +83,9 @@ define(
           self.editors[k].getSession().setAnnotations(annot_bymodule[k]);
         });
       };
-      
+      // This appends all warnings and errors that are not tied to any particular document to the set of errors and
+      // warnings that should be reported on the 'current' editor so that this information is not lost and the user 
+      // can see it in an appropriate way
       function mergeUnboundAnnotationsToCurrentEditor(self, annot_bymodule){
         var annot_of_current = annot_bymodule[self.app.current_module] || [];
         var not_bound_to_mod = _.filter(_.keys(annot_bymodule), function(k){return k<0;});
@@ -60,11 +94,12 @@ define(
         }, annot_of_current);
         annot_bymodule[self.app.current_module] = annot_of_current; 
       };
-      
+      // this adds a 'badge' to the tabs in the navigator to emphasize the fact that some errors and warning have
+      // been reported on those modules.
       function addBadge(self, k, n){
         $(self.navigator.find("li a")[k]).append("<span class='badge' style='float: right;'>"+n+"</span>");
       };
-      
+      // this takes an error as formatted by the backend and translates it in an annotation understood by ACE editor.
       function mkAnnot(kind, error){
          return {
                module: error.pos.module, 
@@ -73,7 +108,9 @@ define(
                type  : kind
          }; 
       };
-      
+      // This refreshes the content of the editors (and the amount of visible editors) from the underlying model.
+      // That is to say, it redraws the complete MultiEditor to make sure all modules are visible and they have 
+      // the correct names.
       function update(self){
           _.each(self.editors, function(e){
               e.destroy();
@@ -86,7 +123,8 @@ define(
             self.tag.find('.invisible').removeClass('invisible');
           }
       };
-      
+      // updates the title associated with any given editor so that they all reflect the name that was given 
+      // in the module definition
       function update_titles(self){
          var entries = self.navigator.find("li > a > span.multied-entry-text");
          $.each(self.app.modules, function(i, module){
@@ -94,7 +132,7 @@ define(
              $(entries[i]).text(title);
          });
       };
-      
+      // This create one editor for each module in the model.
       function mkEditors(self){
           var editors = [];
           for(var i = 0; i<self.app.modules.length; i++){
@@ -102,7 +140,7 @@ define(
           }
           return editors;
       };
-      
+      // This create an editor widget to represent (and edit) the content of the module at index i.
       function mkEditor(self, i){
         var editor    = new Editor();
           
@@ -118,7 +156,8 @@ define(
         editor.getSession().setValue(self.app.modules[i]);
         return editor;
       };
-      
+      // This function creates the navigator tab (left pane) that allows the user to switch between
+      // the different modules he's editing.
       function mkNavigator(self){
           var $nav = $("<ul class='nav nav-pills nav-stacked gray'></ul>");
           self.navigator = $nav;
@@ -144,7 +183,8 @@ define(
           activate(self, self.app.current_module);
           return $nav;
       };
-      
+      // This function activates the editor at index 'index'. This means that the editor
+      // currently open is closed and the one at the index pointed by the user is made open
       function activate(self, index){
         $(self).trigger("changed:current_module", index);
         self.navigator.find("li").removeClass("active");
@@ -152,7 +192,7 @@ define(
         $(entry).addClass("active");
         self.tag.find("[data-name='editor']").html(self.editors[index].tag);
       };
-      
+      // This creates the html structure used to render this widget on screen.
       function mkTag(){
           var tag = "<table style='width: 100%; height: 100%'>"  +
                     "<tr><td class='hidable invisible' style='width: 15%; vertical-align: top; line-height: 100%;' data-name='navigator'></td>" +
@@ -164,7 +204,8 @@ define(
           
           return $(tag);
       };
-      
+      // this function enables the resizing of the navigator pane. That way, the user can adjust the size it takes
+      // to better fit his taste.
       function enableResizing(self){
         /* This is the only way I found to get the x,y coordinates in firefox */
         $(document).on("dragover", function(e){
@@ -184,7 +225,11 @@ define(
         });
       };
       
-      
+     // This function uses the module statement in the body of a module text to determine the title to be given to
+     // an editor.
+     // NOTE: This function has been copied over in the backend (see module instance_resolution.js) so, whenever you
+     //       make changes in the definition of this function, you want to make sure to also reflect those changes 
+     //       at that place too. 
      function module_title(module){
         var exp = /module\s+([^\s]+)/;
         var ret = exp.exec(module);
